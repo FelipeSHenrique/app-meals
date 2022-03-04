23bf224c5bf66f60ce83f266fe03a7b36fda6cc8
Felipe Henrique <felipeh.soares3@gmail.com>
2022-03-03 22:38:41 -0300

    add favorite

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   Settings settings = Settings();
   List<Meal> _availableMeals = DUMMY_MEALS;
   List<Meal> _favoriteMeals = [];
@@ -34,9 +33,24 @@ class _MyAppState extends State<MyApp> {
         final filterVegan = settings.isVegan && !meal.isVegan;
         final filterVegetarian = settings.isVegetarian && !meal.isVegetarian;
 
-        return !filterGluten && !filterLactose && !filterVegan && !filterVegetarian;
+        return !filterGluten &&
+            !filterLactose &&
+            !filterVegan &&
+            !filterVegetarian;
       }).toList();
     });
+  }
+
+  void _toggleFavorite(Meal meal) {
+    setState(() {
+      _favoriteMeals.contains(meal)
+          ? _favoriteMeals.remove(meal)
+          : _favoriteMeals.add(meal);
+    });
+  }
+
+  bool _isFavorite(Meal meal) {
+    return _favoriteMeals.contains(meal);
   }
 
   @override
@@ -44,23 +58,20 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.pink).copyWith(secondary: Colors.amber),
-        fontFamily: 'Raleway',
-        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
-        textTheme: ThemeData.light().textTheme.copyWith(
-          headline6: const TextStyle(
-            fontSize: 20,
-            fontFamily: 'RobotoCondensed'
-          )
-        )
-      ),
+          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.pink)
+              .copyWith(secondary: Colors.amber),
+          fontFamily: 'Raleway',
+          canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+          textTheme: ThemeData.light().textTheme.copyWith(
+              headline6: const TextStyle(
+                  fontSize: 20, fontFamily: 'RobotoCondensed'))),
       routes: {
         AppRoutes.HOME: (ctx) => TabScreen(_favoriteMeals),
-        AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(_availableMeals),
-        AppRoutes.MEAL_DETAIL: (ctx) => MealDetailScreen(),
+        AppRoutes.CATEGORIES_MEALS: (ctx) =>
+            CategoriesMealsScreen(_availableMeals),
+        AppRoutes.MEAL_DETAIL: (ctx) => MealDetailScreen(_toggleFavorite, _isFavorite),
         AppRoutes.SETTINGS: (ctx) => SettingsScreen(settings, _filterMeals),
       },
-
     );
   }
 }
--- lib/screens/favorite_screen.dart
+++ lib/screens/favorite_screen.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:meals/components/meal_item.dart';
 import 'package:meals/models/meal.dart';
 
 class FavoriteScreen extends StatelessWidget {
-
   final List<Meal> favoriteMeals;
 
-  const FavoriteScreen(this.favoriteMeals, {Key? key}): super(key: key);
+  const FavoriteScreen(this.favoriteMeals, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Minhas Refeições Favoritas'),
-    );
+    if (favoriteMeals.isEmpty) {
+      return const Center(
+        child: Text('Minhas Refeições Favoritas'),
+      );
+    } else {
+      return ListView.builder(
+        itemCount: favoriteMeals.length,
+        itemBuilder: (ctx, index) {
+          return MealItem(favoriteMeals[index]);
+        },
+      );
+    }
   }
 }
